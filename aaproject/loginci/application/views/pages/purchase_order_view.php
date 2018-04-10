9c039c5d943ca8c8157d33694204726073100c9c
Sofiathe1st <johncharlesvillavicencio@gmail.com>
2018-04-10 10:49:07 +0800

    import

--- aaproject/loginci/application/views/pages/purchase_order_view.php
+++ aaproject/loginci/application/views/pages/purchase_order_view.php
@@ -1,39 +1,34 @@
-<link rel="stylesheet" href="http://cdn.datatables.net/1.10.11/css/jquery.dataTables.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-<script src="http://cdn.datatables.net/1.10.11/jquery.dataTables.min.js"></script>
-<script>
-    $(document) .ready(function()){
-        $('#example').DataTable();
-
-    });
-</script>
+<html>
 <title>Purchase Orders</title>
+
+<head>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+<script>
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+</script>
+</head>
 	<div id="inv_form">
     <h2 id="inv_title">Purchase Orders</h2> 
-    <a href="<?php echo base_url() ?>index.php/purchase_order" id="a_add">Create New Purchase Order</a>
-<body>
-    <div class="table-responsive">  
-        <div class="table-responsive"><!--this is used for responsive display in mobile and other devices-->  
-  
-  <div id="content">
-    <div class="container">
-        <div class="row">
-            <div class="span12">
+    <a href="<?php echo base_url() ?>index.php/purchase_order" id="a_add">Create New Purchase Order</a><br><br>
+<table id="table_id" class="display">
 
-    <table cellpadding="0" cellspacing="0" border="0" class="table table-striped" id="example"> 
         <thead>  
     
         <tr>  
-            <th><strong>PO no.</strong></th>
-            <th><strong>PO Order Date</strong></th>
-            <th><strong>Manufacturer</strong></th>
-            <th><strong>Remarks</strong></th>
-            <th><strong>Options</strong></th>
-            <!--<th><strong>Add Details</strong></th>-->
+            <th>PO no.</th>
+            <th>PO Order Date</th>
+            <th>Manufacturer</th>
+            <th>Remarks</th>
+            <th>Options</th>
             
 
         </tr>
         </thead>
+        <tbody>
 
             <?php if(is_array($purchase_order_list)) { 
 
@@ -44,14 +39,13 @@
                     <td><?php echo htmlspecialchars($row->purchase_order_date); ?></td>
                     <td><?php echo htmlspecialchars($row->manufacturer); ?></td>
                     <td><?php echo htmlspecialchars($row->remarks); ?></td>
-                    <td><a href="<?php echo base_url() ?>index.php/login/editpurchaseorder/?upd=<?php echo $row->id ?>" id="a_upd">Edit</a>
-                        | <a href="<?php echo base_url() ?>index.php/login/deletepurchaseorder/?del=<?php echo $row->id ?>" id="a_del">Delete</a></td>
-                    <!--<td><a href="<?php echo base_url() ?>index.php/login/inventory/<?php echo $row->serial_number ?>" id="a_upd">Add Details</a></td>-->
-
+                    <!-- <td><a href="<?php echo base_url() ?>index.php/login/editpurchaseorder/?upd=<?php echo $row->id ?>" id="a_upd">Edit</a>
+                        | <a href="<?php echo base_url() ?>index.php/login/deletepurchaseorder/?del=<?php echo $row->id ?>" id="a_del">Delete</a></td> -->
                 </tr> 
                    
 
           <?php      }
 
-            } ?>   
+            } ?>
+            </tbody>   
     </table>
