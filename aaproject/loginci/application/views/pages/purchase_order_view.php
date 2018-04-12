76ecfb159c08eb0d75df8a2bdc5c405f56f66d62
Sofiathe1st <johncharlesvillavicencio@gmail.com>
2018-04-12 08:31:36 +0800

    imported files

--- aaproject/loginci/application/views/pages/purchase_order_view.php
+++ aaproject/loginci/application/views/pages/purchase_order_view.php
@@ -47,7 +47,7 @@ $(document).ready( function () {
                     <td><?php echo htmlspecialchars($row->manufacturer); ?></td>
                     <td><?php echo htmlspecialchars($row->remarks); ?></td>
                     <td><a href="<?php echo base_url() ?>index.php/login/edit_purchase_order_details/<?php echo $row->id ?>" id="a_upd" target="_blank">EDIT</a>
-                        | <a href="<?php echo base_url() ?>index.php/login/deletepurchaseorder/?del=<?php echo $row->id ?>" id="a_del">Delete</a></td>
+                        | <a href="<?php echo base_url() ?>index.php/login/deletepurchaseorder/?del=<?php echo $row->id ?>" id="a_del">DELETE</a></td>
                 </tr> 
                    
 
