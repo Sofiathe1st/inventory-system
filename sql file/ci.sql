bf47e15193dfd0ea6a5c9cefeee6ee2d26e7df34
Sofiathe1st <johncharlesvillavicencio@gmail.com>
2018-04-12 08:38:15 +0800

    updated the .sql file

--- sql file/ci.sql
+++ sql file/ci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2018 at 02:39 AM
+-- Generation Time: Apr 12, 2018 at 02:36 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.0.15
 
@@ -42,10 +42,11 @@ CREATE TABLE `manufacturer` (
 --
 
 INSERT INTO `manufacturer` (`id`, `manufacturer_name`, `contact_no`, `region`, `province`, `city`, `address`, `status`) VALUES
-(1, 'samsung', '12351212313', 'region I', 'region I', 'region I', 'region I', 'not-active'),
-(2, 'toshiba', '124112414', 'region I', 'region I', 'region I', 'region I', 'active'),
-(3, 'asus', '124141414', 'region I', 'region I', 'region I', 'region I', 'not-active'),
-(4, 'acer', '1231241512', 'region I', 'region I', 'region I', 'region I', 'active');
+(1, 'samsung', '12351212313', 'Region 1', 'Region 1', 'Region 1', 'Region 1', 'active'),
+(2, 'toshiba', '124112414', 'Region 1', 'Region 1', 'Region 1', 'Region 1', 'active'),
+(3, 'asus', '124141414', 'Region 1', 'Region 1', 'Region 1', 'Region 1', 'active'),
+(4, 'acer', '1231241512', 'Region 1', 'Region 1', 'Region 1', 'Region 1', 'active'),
+(5, 'hewlett packard', '12313', 'Region 1', 'Region 1', 'Region 1', 'Region 1', 'active');
 
 -- --------------------------------------------------------
 
@@ -69,46 +70,8 @@ CREATE TABLE `purchase_order` (
 --
 
 INSERT INTO `purchase_order` (`id`, `purchase_order_no`, `purchase_order_date`, `manufacturer`, `remarks`, `author_email`, `author_firstname`, `author_lastname`) VALUES
-(1, 'asd', '2018-03-22', 'samsung', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(2, '999', '2018-03-23', 'toshiba', 'sdsdd', 'super@admin', '1John Charles', 'Villavicencio'),
-(3, '0000001', '2018-03-23', 'toshiba', 'ASSD', 'super@admin', '1John Charles', 'Villavicencio'),
-(4, 'asdad', '2018-03-23', 'toshiba', 'a', 'super@admin', '1John Charles', 'Villavicencio'),
-(5, '1234', '2018-04-02', 'samsung', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(6, '1234', '2018-04-02', 'samsung', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(7, '9087', '2018-04-02', 'samsung', 'rdt', 'super@admin', '1John Charles', 'Villavicencio'),
-(8, '0998', '2018-04-02', 'samsung', 'asasd', 'super@admin', '1John Charles', 'Villavicencio'),
-(9, '12309123', '2018-04-02', 'toshiba', 'sdf', 'super@admin', '1John Charles', 'Villavicencio'),
-(10, '12342', '2018-04-02', 'samsung', 's', 'super@admin', '1John Charles', 'Villavicencio'),
-(11, '1235', '2018-04-02', 'toshiba', 'sdf', 'super@admin', '1John Charles', 'Villavicencio'),
-(12, '123', '2018-04-02', 'samsung', 'dfs', 'super@admin', '1John Charles', 'Villavicencio'),
-(13, '12', '2018-04-02', 'toshiba', 'sf', 'super@admin', '1John Charles', 'Villavicencio'),
-(14, '123455', '2018-04-02', 'toshiba', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(15, '32425', '2018-04-02', 'toshiba', 'sdf', 'super@admin', '1John Charles', 'Villavicencio'),
-(16, '123131', '2018-04-02', 'samsung', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(17, '9980', '2018-04-03', 'samsung', 'XC', 'super@admin', '1John Charles', 'Villavicencio'),
-(18, '1235', '2018-04-03', 'samsung', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(19, 'sasdaddf', '2018-04-03', 'toshiba', 'sdfsf', 'super@admin', '1John Charles', 'Villavicencio'),
-(20, '124', '2018-04-03', 'toshiba', 'aad', 'super@admin', '1John Charles', 'Villavicencio'),
-(21, '1245', '2018-04-03', 'samsung', 'asd', 'super@admin', '1John Charles', 'Villavicencio'),
-(22, '123573', '2018-04-03', 'samsung', 'asdw', 'super@admin', '1John Charles', 'Villavicencio'),
-(23, 'lknln', '2018-04-03', 'samsung', 'qwerty', 'super@admin', '1John Charles', 'Villavicencio'),
-(24, '1231412414', '2018-04-03', 'toshiba', 'sfaf', 'super@admin', '1John Charles', 'Villavicencio'),
-(25, 'llk', '2018-04-03', 'samsung', 'aasd', 'super@admin', '1John Charles', 'Villavicencio'),
-(26, '1245', '2018-04-03', 'samsung', 'adasd', 'super@admin', '1John Charles', 'Villavicencio'),
-(27, '09o9', '2018-04-03', 'toshiba', 'sada', 'admin@admin', '2John Charles', 'Villavicencio'),
-(28, 'asd', '2018-04-03', 'toshiba', 'sfaf', 'admin@admin', '2John Charles', 'Villavicencio'),
-(29, '1231', '2018-04-03', 'samsung', 'asd', 'admin@admin', '2John Charles', 'Villavicencio'),
-(30, '1231', '2018-04-03', 'samsung', 'asd', 'admin@admin', '2John Charles', 'Villavicencio'),
-(31, 'adasd', '2018-04-03', 'asus', 'asdad', 'admin@admin', '2John Charles', 'Villavicencio'),
-(32, '123', '2018-04-03', 'toshiba', 'ssf', 'admin@admin', '2John Charles', 'Villavicencio'),
-(33, 'wy6', '2018-04-03', 'samsung', 'asd', 'admin@admin', '2John Charles', 'Villavicencio'),
-(34, 'fafag', '2018-04-03', 'toshiba', 'afgfgdg', 'admin@admin', '2John Charles', 'Villavicencio'),
-(35, '12e', '2018-04-04', 'toshiba', 'sdf', 'super@admin', '1John Charles', 'Villavicencio'),
-(36, '1123', '2018-04-04', 'toshiba', 'as', 'super@admin', '1John Charles', 'Villavicencio'),
-(37, 'asdaf', '2018-04-04', 'samsung', 'sdfsf', 'super@admin', '1John Charles', 'Villavicencio'),
-(38, 'asda', '2018-04-04', 'samsung', 'df', 'super@admin', '1John Charles', 'Villavicencio'),
-(39, '1131', '2018-04-04', 'samsung', 'sda', 'super@admin', '1John Charles', 'Villavicencio'),
-(40, '09', '2018-04-05', 'samsung', 'jkj', 'super@admin', '1John Charles', 'Villavicencio');
+(39, '11312', '2018-04-04', 'toshiba', 'sda', 'super@admin', '1John Charles', 'Villavicencio'),
+(41, '9698', '2018-04-11', 'toshiba', 'adasd', 'super@admin', '1John Charles', 'Villavicencio');
 
 -- --------------------------------------------------------
 
@@ -140,18 +103,6 @@ INSERT INTO `purchase_order_details` (`serial_number`, `purchase_order_no`, `col
 -- --------------------------------------------------------
 
 --
--- Table structure for table `purchase_order_no`
---
-
-CREATE TABLE `purchase_order_no` (
-  `id` int(11) NOT NULL,
-  `date` varchar(50) NOT NULL,
-  `purchase_order_no` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
@@ -175,8 +126,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `middlename`, `type`, `department`, `username`, `password`, `email`, `dateregistered`) VALUES
 (00017, '1John Charles', 'Villavicencio', 'Bautista', 'SuperAdmin', 'CT-DEPARTMENT', 'superadmin', '17c4520f6cfd1ab53d8745e84681eb49', 'super@admin', '2018-03-06'),
 (00077, '3John Charles', 'Villavicencio', 'Bautista', 'User', 'CT-DEPARTMENT', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user@user', '2018-03-06'),
-(00078, '2John Charles', 'Villavicencio', 'Bautista', 'Admin', 'CT-DEPARTMENT', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin', '2018-03-08'),
-(00079, 'John Charles', 'Villavicencio', 'Bautista', 'SuperAdmin', 'CTD-DEPARTMENT', 'asd', '7815696ecbf1c96e6894b779456d330e', 'goku@ukog', '2018-04-02');
+(00078, '2John Charles', 'Villavicencio', 'Bautista', 'Admin', 'CT-DEPARTMENT', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin', '2018-03-08');
 
 --
 -- Indexes for dumped tables
@@ -195,12 +145,6 @@ ALTER TABLE `purchase_order`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `purchase_order_no`
---
-ALTER TABLE `purchase_order_no`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -214,22 +158,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `manufacturer`
 --
 ALTER TABLE `manufacturer`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `purchase_order`
 --
 ALTER TABLE `purchase_order`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
---
--- AUTO_INCREMENT for table `purchase_order_no`
---
-ALTER TABLE `purchase_order_no`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
