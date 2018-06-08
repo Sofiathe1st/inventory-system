bf78fb67dc47d6b6c74e25cfd85ce83da68dd6a7
Sofiathe1st <johncharlesvillavicencio@gmail.com>
2018-06-08 18:42:51 +0800

    test reupload

--- sql file/ci.sql
+++ sql file/ci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2018 at 08:03 AM
+-- Generation Time: Jun 08, 2018 at 12:40 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.0.15
 
@@ -46,14 +46,9 @@ CREATE TABLE `purchase_order` (
 --
 
 INSERT INTO `purchase_order` (`id`, `purchase_order_no`, `purchase_order_date`, `category`, `supplier`, `quantity`, `item_type`, `price`, `remarks`, `author_email`, `author_firstname`, `author_lastname`) VALUES
-(75, '1213', '2018-05-04', 'Other', 'LAZADA', 2, 'Not Applicable', 16000, 'asd', 'super@admin', 'John Charles', 'Villavicencio'),
-(76, '0001', '2018-05-04', 'Hardware', 'LAZADA', 2, 'Computer', 50000, 'i7 4Gb RAM 500Gb HDD', 'super@admin', 'John Charles', 'Villavicencio'),
-(80, '123123', '2018-05-04', 'Hardware', 'LAZADA', 2, '3', 23, 'asd', 'super@admin', 'John Charles', 'Villavicencio'),
-(84, '233', '2018-05-21', 'Software', 'LAZADA', 2, 'LCD Monitor', 50000, 'asd', 'super@admin', 'John Charles', 'Villavicencio'),
-(85, '12333', '2018-05-21', 'Hardware', 'HMR', 2, 'laptop', 15000, 'asd', 'super@admin', 'John Charles', 'Villavicencio'),
-(86, '1', '2018-05-21', 'Hardware', 'HMR', 20, '23', 123123, 'ad', 'super@admin', 'John Charles', 'Villavicencio'),
-(87, '002', '2018-05-22', 'Hardware', 'HMR', 2, 'laptop', 2000, 'asd', 'super@admin', 'John Charles', 'Villavicencio'),
-(88, '003', '2018-05-22', 'Hardware', 'HMR', 12, 'asd', 1231231, 'aasd', 'super@admin', 'John Charles', 'Villavicencio');
+(85, '001', '2018-05-21', 'Hardware', 'HMR', 2, 'laptop', 15000, 'asd', 'super@admin', 'John Charles', 'Villavicencio'),
+(87, '002', '2018-05-22', 'Hardware', 'HMR', 2, 'laptop', 2000, 'asd', 'admin@admin', 'John Charles', 'Villavicencio'),
+(90, '003', '2018-05-23', 'Hardware', 'LAZADA', 2, 'ad', 990, 'asd', 'user@user', 'John Charles', 'Villavicencio');
 
 -- --------------------------------------------------------
 
@@ -88,15 +83,10 @@ CREATE TABLE `purchase_order_details` (
 --
 
 INSERT INTO `purchase_order_details` (`id`, `purchase_order_no`, `purchase_order_date`, `category`, `serial_number`, `name_sh`, `manufacturer`, `quantity`, `assigned_to`, `item_type`, `specifications`, `price`, `supplier`, `status`, `color`, `remarks`, `author_firstname`, `author_lastname`, `author_email`) VALUES
-(1, '0003', '2018-05-04', 'Hardware', '00327-35038-41362-AAOEM', 'ASUS X405U', 'ASUS', 1, 'John Charles Villavicencio', 'Laptop', 'i5-7200U\r\n4gb RAM\r\n1TB HDD\r\nWIN10', 20000, 'LAZADA', 'Working', 'Gray', 'Request 1 new laptop for new employee.', 'John Charles', 'Villavicencio', 'super@admin'),
-(17, 'asd2', '2018-05-04', 'Other', '0233-123-3453-234s-234d', 'Samsung Galaxy Note S3 Neo ', 'Samsung', 1, 'John Charles Villavicencio', 'Phone', '2GB RAM\nAndroid 10.2\n', 12500, 'LAZADA', 'Spare', 'Green', 'ad123', 'John Charles', 'Villavicencio', 'super@admin'),
-(18, 'asd2', '2018-05-04', 'Other', '0233-123-3453-234s-221a', 'Samsung Galaxy Note S3 Neo ', 'Samsung', 1, 'John Charles Villavicencio', 'Phone', '2GB RAM\nAndroid 10.2\n', 12500, 'LAZADA', 'Defective', 'Pink', 'ad123', 'John Charles', 'Villavicencio', 'super@admin'),
-(19, '0001', '2018-05-04', 'Hardware', '12', 'asd', 'asd', 1, 'asd', 'Computer', 'asd', 25000, 'LAZADA', 'Working', 'asd', 'i7 4Gb RAM 500Gb HDD', 'John Charles', 'Villavicencio', 'super@admin'),
-(20, '0001', '2018-05-04', 'Hardware', '123', 'asd', 'asd', 1, 'asd', 'Computer', 'asd', 25000, 'LAZADA', 'Working', 'asd', 'i7 4Gb RAM 500Gb HDD', 'John Charles', 'Villavicencio', 'super@admin'),
+(19, '001', '2018-05-04', 'Hardware', '12', 'asd', 'asd', 1, 'asd', 'Computer', 'asd', 25000, 'LAZADA', 'Working', 'asd', 'i7 4Gb RAM 500Gb HDD', 'John Charles', 'Villavicencio', 'super@admin'),
+(20, '001', '2018-05-04', 'Hardware', '123', 'asd', 'asd', 1, 'asd', 'Computer', 'asd', 25000, 'LAZADA', 'Working', 'asd', 'i7 4Gb RAM 500Gb HDD', 'John Charles', 'Villavicencio', 'super@admin'),
 (21, '002', '2018-05-22', 'Hardware', '000231', 'asus 123', 'asus', 1, 'john diaz', 'laptop', 'x405u', 1000, 'HMR', 'Working', 'white', 'asd', 'John Charles', 'Villavicencio', 'super@admin'),
-(22, '002', '2018-05-22', 'Hardware', '000234', 'hp 123', 'hp', 1, 'jovert galang', 'laptop', 'x405u', 1000, 'HMR', 'Working', 'gray', 'asd', 'John Charles', 'Villavicencio', 'super@admin'),
-(23, '002', '2018-05-22', 'Hardware', '12313', 'asd', 'qwe', 1, 'asd', 'laptop', 'asdad', 1000, 'HMR', 'Working', 'asd', 'asd', 'John Charles', 'Villavicencio', 'super@admin'),
-(24, '002', '2018-05-22', 'Hardware', '12313', 'asd', 'asd', 1, 'asd', 'laptop', 'asd', 1000, 'HMR', 'Working', 'asd', 'asd', 'John Charles', 'Villavicencio', 'super@admin');
+(22, '002', '2018-05-22', 'Hardware', '000234', 'hp 123', 'hp', 1, 'jovert galang', 'laptop', 'x405u', 1000, 'HMR', 'Working', 'gray', 'asd', 'John Charles', 'Villavicencio', 'super@admin');
 
 -- --------------------------------------------------------
 
@@ -187,12 +177,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `purchase_order`
 --
 ALTER TABLE `purchase_order`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 --
 -- AUTO_INCREMENT for table `purchase_order_details`
 --
 ALTER TABLE `purchase_order_details`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `supplier`
 --
@@ -202,7 +192,7 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
